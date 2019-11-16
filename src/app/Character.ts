6c16d8035fd568db4f87260ddd1e2e4f6a308747
vugluskr86 <ivashkin.vasiliy@gmail.com>
2019-11-16 20:37:12 +0500

    prettier

--- src/app/Character.ts
+++ src/app/Character.ts
@@ -1,11 +1,11 @@
-import { Sprite, Texture } from 'pixi.js';
+import { Sprite, Texture } from 'pixi.js'
 
 export class Character {
-  public sprite: Sprite;
+  public sprite: Sprite
 
   constructor(public texture: Texture) {
-    this.sprite = new Sprite(texture);
-    this.sprite.anchor.y = 0.5;
-    this.sprite.anchor.x = 0.5;
+    this.sprite = new Sprite(texture)
+    this.sprite.anchor.y = 0.5
+    this.sprite.anchor.x = 0.5
   }
 }
