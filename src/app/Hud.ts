d38619115f6762b779053a70a9d59bb3100cfb15
Kiborgik <black_don@ukr.net>
2019-11-16 17:47:25 +0200

    add drawText

--- src/app/Hud.ts
+++ src/app/Hud.ts
@@ -1,4 +1,4 @@
-import { Container, Graphics, DisplayObject } from "pixi.js";
+import { Container, Graphics, DisplayObject, TextStyle, Text } from "pixi.js";
 
 export class Hud extends DisplayObject {
   public object: Container;
@@ -22,5 +22,24 @@ export class Hud extends DisplayObject {
     bar.drawRect(0, 0, width, height);
     bar.endFill();
     object.addChild(bar);
+  }
+  public drawText(
+    x: number,
+    y: number,
+    text: string,
+    textSize: number,
+    color: number
+  ): void {
+    const style = new TextStyle({
+      fontFamily: "Futura",
+      fontSize: textSize,
+      fill: color,
+      dropShadow: true,
+      dropShadowDistance: 1
+    });
+    const message = new Text(text, style);
+    message.x = x;
+    message.y = y;
+    this.object.addChild(message);
   }
 }
