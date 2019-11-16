47bfc0b815530ffe276b9f58ad7428a7f494444a
vugluskr86 <ivashkin.vasiliy@gmail.com>
2019-11-16 17:59:32 +0500

    add camera

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,39 @@
 import { Application, Loader } from 'pixi.js';
+
+import { Viewport } from 'pixi-viewport';
+
 import { Character } from '@app/Character';
 import { Room } from '@app/Room';
 
 class Game {
   private app: Application;
   private loader: Loader;
+  private viewport: Viewport;
 
   constructor() {
-    // instantiate app
-    this.app = new Application({
-      backgroundColor: 0x1099bb, // light blue
-      height: 600,
-      width: 800
-    });
+    this.app = new Application({backgroundColor: 0x1099bb});
+    document.body.appendChild(this.app.view);
 
     this.loader = new Loader();
 
-    // create view in DOM
-    document.body.appendChild(this.app.view);
+    this.viewport = new Viewport({
+      interaction: this.app.renderer.plugins.interaction,
+      screenHeight: 720,
+      screenWidth: 1280,
+      worldHeight: 2000,
+      worldWidth: 2000
+    });
+
+    this.app.stage.addChild(this.viewport);
+
+    this.viewport
+      .drag()
+      .pinch()
+      .wheel()
+      .decelerate();
 
     // preload needed assets
-    this.loader.add('samir', '/assets/img/hero.png');
+    this.loader.add('hero', '/assets/img/hero.png');
     this.loader.add('room', '/assets/img/room_empty.png');
 
     // then launch app
@@ -32,12 +45,12 @@ class Game {
     // room
     const room = new Room(this.loader.resources['room'].texture);
     const roomSprite = room.sprite;
-    this.app.stage.addChild(roomSprite);
+    this.viewport.addChild(roomSprite);
 
     // append hero
-    const hero = new Character(this.loader.resources['samir'].texture);
+    const hero = new Character(this.loader.resources['hero'].texture);
     const heroSprite = hero.sprite;
-    this.app.stage.addChild(heroSprite);
+    this.viewport.addChild(heroSprite);
     heroSprite.y = 300;
 
     //  animate hero
