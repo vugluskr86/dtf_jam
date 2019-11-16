1c339c99fca73ea2728603fce98ce5e56be8c231
vugluskr86 <ivashkin.vasiliy@gmail.com>
2019-11-16 18:45:45 +0500

    update typescript

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,50 +1,53 @@
+
+/* tslint:disable */
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+/* tslint:enable */
 
 module.exports = {
-  entry: './src/app.ts',
-  devtool: 'inline-source-map',
-  mode: 'development',
   devServer: {
     contentBase: './dist',
     hot: true,
-    port: 4200
+    port: 4200,
   },
-  plugins: [
-    new CleanWebpackPlugin(['dist']),
-    new HtmlWebpackPlugin({
-      title: 'Development'
-    }),
-    new CopyWebpackPlugin([{ from: './src/assets', to: 'assets' }]),
-    new webpack.NamedModulesPlugin(),
-    new webpack.HotModuleReplacementPlugin()
-  ],
+  devtool: 'inline-source-map',
+  entry: './src/app.ts',
+  mode: 'development',
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'awesome-typescript-loader'
+        use: 'awesome-typescript-loader',
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: 'file-loader'
-      }
-    ]
-  },
-  resolve: {
-    plugins: [
-      new TsconfigPathsPlugin({
-        /*configFile: "./path/to/tsconfig.json" */
-      })
+        use: 'file-loader',
+      },
     ],
-    extensions: ['.tsx', '.ts', '.js']
   },
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new CleanWebpackPlugin(['./dist']),
+    new HtmlWebpackPlugin({
+      title: 'Development',
+    }),
+    new CopyWebpackPlugin([{ from: './src/assets', to: 'assets' }]),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    plugins: [
+      new TsconfigPathsPlugin({
+        configFile: './tsconfig.json',
+      }),
+    ],
+  },
 };
