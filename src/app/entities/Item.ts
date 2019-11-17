75de0af3cf3e7c7246c649e46e40c1a63642931b
vugluskr86 <ivashkin.vasiliy@gmail.com>
2019-11-18 00:09:53 +0500

    styles

--- src/app/entities/Item.ts
+++
@@ -1,9 +0,0 @@
-import { Sprite, Texture } from 'pixi.js';
-
-export class Item extends Sprite {
-  constructor(public texture: Texture) {
-    super(texture);
-    this.anchor.y = 0.5;
-    this.anchor.x = 0.5;
-  }
-}
